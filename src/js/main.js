c402b75e729b618c6f67d5b476794b8821b08b8e
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-02-04 21:08:35 -0600

    feat: add create input element

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 import {test} from './agregarTarea.js';
 import {eliminar} from './eliminarTarea.js';
 
-console.log(test, eliminar)
+let input = document.createElement("input");
+input.setAttribute("id", "input");
+input.setAttribute("type", "text");
+input.setAttribute("placeholder", "Agrega una nueva tarea");
+
+document.getElementById("app").appendChild(input);
