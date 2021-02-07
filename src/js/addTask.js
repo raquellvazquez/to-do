39cc0182f31b7603c7fab77d750de0f5c9f65720
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-02-07 16:52:41 -0600

    eliminate styles of js file

--- src/js/addTask.js
+++ src/js/addTask.js
@@ -12,29 +12,9 @@ export function addTask() {
       <p class='nameTask'>
       ${inputValue} 
       </p> 
-      <button class='close' type='button'>Eliminar</button>
+      <button class='eliminar' type='button'>Eliminar</button>
       </li>`;
     document.getElementById("list").innerHTML += task;
-    var li = document.getElementsByTagName("li");
-
-    for (var i = 0; i < li.length; i++) {
-      li[i].setAttribute(
-        "style",
-        `display: flex; 
-                                  justify-content: space-around; 
-                                  align-items: center; width: 700px; 
-                                  height: 50px; 
-                                  background: rgb(255, 255, 255); 
-                                  border: 1px solid rgb(234, 240, 247); 
-                                  box-shadow: 0px 0px 20px -10px rgba(0, 0, 0, 0.3); 
-                                  border-radius: 10px; margin: 16px`
-      );
-    }
-    var pElement = document.getElementsByClassName("nameTask");
-
-    for (var i = 0; i < pElement.length; i++) {
-      pElement[i].setAttribute("style", "margin: 0; font-size: 1.5em");
-    }
     input.value = "";
   }
 
